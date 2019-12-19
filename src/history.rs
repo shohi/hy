55fb4e21a6f40854c1e5aa2d0bc51b3f5f9e8473
shohi <oshohi@gmail.com>
2019-12-19 22:55:03 +0800

    update history.rs

--- src/history.rs
+++ src/history.rs
@@ -1,33 +1,96 @@
+use chrono::Local;
+use shellexpand;
+use std::fs::{self, File, OpenOptions};
+use std::io::{self, Write};
+use std::io::{BufRead, BufReader};
+use std::path::Path;
+
+const HIST_BASEDIR: &'static str = "~/.config/hy";
+const HIST_FILENAME: &'static str = "history";
+
+fn get_history_file() -> io::Result<File> {
+    let p = shellexpand::tilde(HIST_BASEDIR).into_owned();
+    let basedir_path = Path::new(p.as_str()).join("history");
+
+    if !basedir_path.exists() {
+        fs::create_dir_all(&p)?;
+    }
+
+    let path_buf = Path::new(p.as_str()).join(HIST_FILENAME);
+    let file = OpenOptions::new()
+        .read(true)
+        .append(true)
+        .create(true)
+        .open(path_buf)?;
+
+    Ok(file)
+}
+
+fn get_max_history_no(f: &File) -> u64 {
+    let file = BufReader::new(f);
+    let line = file.lines().last();
+    let line = match line {
+        Some(s) => s,
+        None => return 0,
+    };
+
+    let line = match line {
+        Ok(s) => s,
+        Err(_) => return 0,
+    };
+
+    let tokens: Vec<&str> = line.split(":").collect();
+    if tokens.len() == 0 {
+        return 0;
+    }
+
+    let max_no = tokens[0].parse::<u64>();
+
+    match max_no {
+        Ok(v) => v,
+        Err(_) => return 0,
+    }
+}
+
+fn record_search(word: &str) {
+    let f = get_history_file();
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => {
+            println!("open history file error: {:?}", e);
+            return;
+        }
+    };
+
+    let dt = Local::now();
+    let max_no = get_max_history_no(&f);
+    let record_no = max_no + 1;
+
+    let result = writeln!(&mut f, "{}:{};{}", record_no, dt.timestamp(), word);
+    match result {
+        Ok(()) => {}
+        Err(e) => {
+            println!("write history error: {:?}", e);
+        }
+    };
+}
+
 #[cfg(test)]
 mod tests {
-    use shellexpand;
-    use std::path::Path;
-    use std::{fs, fs::File};
+    use super::*;
 
     #[test]
     fn test_create_file() {
-        let p = shellexpand::tilde("~/.config/hy").into_owned();
-        println!("base dir: {:?}", &p);
-
-        let path_buf = Path::new(p.as_str()).join("history");
-        let hist_path = path_buf.as_path();
-
-        println!("path: {:?} exist-{:?}", path_buf, hist_path.exists());
-
-        if hist_path.exists() {
-            return;
-        }
-
-        let result = fs::create_dir_all(&p);
-        match result {
-            Ok(()) => println!("create successfully"),
-            Err(e) => println!("err: {:?}", e),
-        }
-
-        let f = File::create(hist_path);
+        let f = get_history_file();
         match f {
             Ok(_) => println!("create ok"),
             Err(e) => println!("err: {:?}", e),
         }
+    }
+
+    #[test]
+    fn test_record_search() {
+        let word: &str = "hello";
+        record_search(word);
     }
 }
